6bd565b601ea0107339c3232ec20cbf6302c6a33
Zandis <zandis@dsistemas.lv>
2021-03-19 23:31:12 +0200

    Temporary visit feature

--- NotifyTemporaryVisitFeature.java
+++ NotifyTemporaryVisitFeature.java
@@ -3,11 +3,14 @@ package org.makeriga.tgbot.features.notifyarrival;
 import org.makeriga.tgbot.MakeRigaTgBot;
 import org.makeriga.tgbot.Settings;
 import org.makeriga.tgbot.features.Feature;
+import org.makeriga.tgbot.features.occupants.OccupantsFeature;
 
 public class NotifyTemporaryVisitFeature extends Feature {
 
     public static final String FEATURE_ID = "notify_temp_visit";
     private static final String CMD__NOTIFY_TEMPORARY_ARRIVAL = "/notify_temporary_visit";
+    
+    private OccupantsFeature occupantsFeature = null;
 
     public NotifyTemporaryVisitFeature(MakeRigaTgBot bot, Settings settings) {
         super(bot, settings);
@@ -18,7 +21,10 @@ public class NotifyTemporaryVisitFeature extends Feature {
         super.Init();
         
         // commands descriptions
-        AddPublicCommandDescription(CMD__NOTIFY_TEMPORARY_ARRIVAL, "notify about your temporary visit (private only)");
+        AddPublicCommandDescription(CMD__NOTIFY_TEMPORARY_ARRIVAL, "notify about your temporary visit");
+        
+        // reference occupants feature
+        occupantsFeature = getBot().getFeatures().containsKey(OccupantsFeature.FEATURE_ID) ? (OccupantsFeature)getBot().getFeatures().get(OccupantsFeature.FEATURE_ID) : null;
     }
     
     @Override
@@ -31,16 +37,14 @@ public class NotifyTemporaryVisitFeature extends Feature {
         if (!testCommand(CMD__NOTIFY_TEMPORARY_ARRIVAL, text))
             return false;
         
-        if (!isPrivateMessage && testCommandWithoutArguments(CMD__NOTIFY_TEMPORARY_ARRIVAL, text)) {
-            sendMessage(chatId, "Private only, please.", messageId);
-            return true;
-        }
-        
-        ArrivalNotification notif = new ArrivalNotification();
-        notif.arrivalAfterMinutes = 5;
-        notif.stayMinutes = 5;
-        notif.memberName = senderTitle;
-        sendMessage(chatId, notif.toString(), null);
+        ArrivalNotification not = new ArrivalNotification();
+        not.arrivalAfterMinutes = 2;
+        not.stayMinutes = 5;
+        not.memberName = senderTitle;
+        sendMessage(chatId, not.toString(), null);
+                    
+        if (occupantsFeature != null)
+            occupantsFeature.RegisterArrival(not.arrivalDate, not.leaveDate, not.extraMembers, senderTitle);
         
         return true;
     }
